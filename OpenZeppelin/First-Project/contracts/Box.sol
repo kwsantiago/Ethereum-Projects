9cd33e0e23f1836de60f226be1890a6ef65bab9a
kwsantiago <kwsantiago@mail.usf.edu>
2020-08-26 18:20:09 -0400

    Add Auth

--- OpenZeppelin/First-Project/contracts/Box.sol
+++ OpenZeppelin/First-Project/contracts/Box.sol
@@ -2,19 +2,27 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.6.0;
 
+// Import Auth from the access-control subdirectory
+import "./Auth.sol";
+
 contract Box{
     uint256 private value;
+    Auth private auth;
 
-    // Emitted when the stored value changes
     event ValueChanged(uint256 newValue);
 
-    // Stores a new value in the contract
+    constructor(Auth _auth) public{
+        auth = _auth;
+    }
+
     function store(uint256 newValue) public{
+        // Require that the caller is registered as an administrator in Auth
+        require(auth.isAdministrator(msg.sender), "Unauthorized");
+
         value = newValue;
         emit ValueChanged(newValue);
     }
 
-    // Reads the last stored value
     function retrieve() public view returns (uint256){
         return value;
     }
